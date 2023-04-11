10178e4ce4545b7c9d8305de14b2e8fd4dd06f52
Rushil Jariwala <rkjjariwala@gmail.com>
2023-04-11 18:48:08 +0530

    prgn 4

--- 8086/block_transfer.asm
+++ 8086/block_transfer.asm
@@ -3,6 +3,11 @@ Program to transfer 1KB from 25000 to 36000
 25000 = 2000x10+5000
 36000 = 3000x10+6000
 
+1KB = (1024)d so covert it to hexadecimal 
+1024 = 0400h remember
+if we are using movsw then the count needs to be reduced to half (even)
+
+
 dosseg
 model small
 .8086
@@ -11,7 +16,7 @@ model small
 a dw 2000h
 b dw 3000h
 c dw 5000h
-d dw 6000h
+d dw 6000h 
 
 .code
 mov ax,a
@@ -20,6 +25,7 @@ mox ax, b
 mov es, ax
 mov si, c
 mov di, d
+mov cx, 0400h
 cld
 rep movsb
 int 03H
