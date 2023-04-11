08c7598a95fba7511c996132472b5bb6e51fcb77
Rushil Jariwala <rkjjariwala@gmail.com>
2023-04-11 19:46:52 +0530

    prgm 7

---
+++ 8086/even_odd.asm
@@ -0,0 +1,28 @@
+42000 is the starting address
+
+dosseg
+model small
+.8086
+
+.code
+mov ax, 4000h
+mov ds, ax
+mov si, 2000h
+mov cx, 0064h
+mov bl, 00H ;even counter
+mov bh, 00H ;odd counter
+back : mov al, [si]
+       ror al, 1
+       jc odd
+       inc bl
+       jmp next
+odd :  inc bh
+next : inc si
+       loop back 
+       mov [si], bx
+int 03H
+end
+
+
+
+
