cf88674cd616a4dd8aaaef8aac896b0481bdc1d6
Rushil Jariwala <rkjjariwala@gmail.com>
2023-04-23 10:56:05 +0530

    max modified code

--- 8086/max.asm
+++ 8086/max.asm
@@ -1,19 +1,20 @@
 dosseg
 model small
 .8086
-100 numbers starting from 42000
+
+;100 numbers starting from 42000 physical address
 
 .code
-mov ax, 4000h
-mov ds, ax
-mov si, 2000h
-mov cx, 0064h
-mov al , 00H
+       mov ax, 4000h
+       mov ds, ax
+       mov si, 2000h
+       mov cx, 000ah ;count of 100d
+       mov al , 00H
 back : cmp al , [si]
        jnc skip
        mov al, [si]
 skip : inc si
        loop back
-mov [si], al
-int 03H
+       mov [si], al
+       int 03h
 end
